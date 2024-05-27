04af0e5ebfcf2c9a84b93bed15bf68a2d151ae9e
Lautiler <147055723+Lautiler@users.noreply.github.com>
2024-05-27 09:05:40 -0300

    9.05

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
 {
+    public InputField inputpassword;
+    public string manzana;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,16 @@ public class LoginManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void CheckPassword()
+    {
+        if (inputpassword.text == manzana)
+        {
+            Debug.Log("acces granted");
+        }
+        else
+        {
+            Debug.Log("Access denied");
+        }
     }
 }
